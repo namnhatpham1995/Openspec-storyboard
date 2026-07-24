22a5af54d7f38dd212ee1bcdd5f636b04a9dc320
namnhatpham1995 <namnhatpham1995@gmail.com>
2026-07-24 06:26:40 +0200

    Modernize concurrency tests

--- internal/watch/watcher_test.go
+++ internal/watch/watcher_test.go
@@ -3,33 +3,31 @@ package watch
 import (
 	"context"
 	"testing"
+	"testing/synctest"
 	"time"
 )
 
 func TestDebounceCoalescesBurst(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	input := make(chan struct{}, 10)
-	output := Debounce(ctx, input, 20*time.Millisecond)
+	synctest.Test(t, func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		input := make(chan struct{}, 10)
+		output := Debounce(ctx, input, 20*time.Millisecond)
 
-	for range 10 {
+		for range 10 {
+			input <- struct{}{}
+		}
+		synctest.Wait()
+		<-output
+
+		synctest.Wait()
+		select {
+		case <-output:
+			t.Fatal("burst produced more than one signal")
+		default:
+		}
+
 		input <- struct{}{}
-	}
-	select {
-	case <-output:
-	case <-time.After(250 * time.Millisecond):
-		t.Fatal("timed out waiting for debounced signal")
-	}
-	select {
-	case <-output:
-		t.Fatal("burst produced more than one signal")
-	case <-time.After(40 * time.Millisecond):
-	}
-
-	input <- struct{}{}
-	select {
-	case <-output:
-	case <-time.After(250 * time.Millisecond):
-		t.Fatal("second burst did not produce a signal")
-	}
+		<-output
+	})
 }
